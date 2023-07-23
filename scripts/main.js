e2fa2d789a898af97dea4bb6cffb21ed5f1bdf6c
RedWilly <106310233+RedWilly@users.noreply.github.com>
2023-07-23 09:02:20 -0400

    Update main.js
    With these changes, the bot will only execute a trade if the profit after deducting the gas cost is greater than zero. This will help ensure that the bot only engages in profitable trades, taking the gas fees into account.

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,11 @@
-const { generateTriads, addPairReserves, calculateProfit, APPROX_GAS_FEE } = require("./arbUtils");
+const { generateTriads, addPairReserves, calculateProfit } = require("./arbUtils");
 const hre = require("hardhat");
 const { ethers, network } = require("hardhat");
-const {
-  PIVOT_TOKEN,
-  SUPER_ARBIT_ADDRESS,
-  MATCHED_PAIRS_OUTPUT_FILE,
-  MAX_GAS,
-  BSC_GAS_PRICE,
-  MAX_TRADE_INPUT,
-} = require("./config");
+const { PIVOT_TOKEN, SUPER_ARBIT_ADDRESS, MATCHED_PAIRS_OUTPUT_FILE, MAX_GAS, MAX_TRADE_INPUT } = require("./config");
 
 let execCount = 0; // Delete later...
 
-const checkProfitAndExecute = async function (lucrPaths, router, signer) {
+const checkProfitAndExecute = async function (lucrPaths, router, signer, gasPrice) {
   console.log("Static batch check starts...");
   const startToken = PIVOT_TOKEN;
   for (const lucrPath of lucrPaths) {
@@ -49,9 +42,8 @@ const checkProfitAndExecute = async function (lucrPaths, router, signer) {
         let gas = await router.estimateGas.superSwap(path.execAmounts, path.execPools, startToken);
         console.log("Gas(static) used: ", gas);
         path.gas = gas.toString();
-        const newProfit = ethers.BigNumber.from(path.expectedProfitBN)
-          .sub(gas.mul(BSC_GAS_PRICE.toString()))
-          .add(APPROX_GAS_FEE);
+        const gasCost = gas.mul(gasPrice);
+        const newProfit = ethers.BigNumber.from(path.expectedProfitBN).sub(gasCost);
         console.log("New Profit", parseFloat(ethers.utils.formatEther(newProfit)));
         if (newProfit.gt(0)) {
           await router.callStatic.superSwap(path.execAmounts, path.execPools, startToken, { gasLimit: MAX_GAS });
@@ -71,6 +63,11 @@ const main = async () => {
   // ---connect to router and other stuff, reorg later---
   const router = await ethers.getContractAt("SuperArbit", SUPER_ARBIT_ADDRESS);
   const signer = await ethers.getSigner();
+
+  // Fetch the current gas price from the BSC network
+  const gasPrice = await ethers.provider.getGasPrice();
+  console.log("Current gas price:", parseFloat(ethers.utils.formatUnits(gasPrice, "gwei")), "gwei");
+
   let triads = generateTriads(MATCHED_PAIRS_OUTPUT_FILE); // generate triads with pivot token -> WBNB
   let allLucrPathsPassed = [];
   while (true) {
@@ -93,7 +90,7 @@ const main = async () => {
       console.log("Length of lucrative triads in current batch:", lucrPaths.length);
       //-------------------------------------
       //--Here comes the check/execute stuff
-      const lucrPathsPassed = await checkProfitAndExecute(lucrPaths, router, signer);
+      const lucrPathsPassed = await checkProfitAndExecute(lucrPaths, router, signer, gasPrice);
       if (lucrPathsPassed.length > 0) allLucrPathsPassed = allLucrPathsPassed.concat(lucrPathsPassed);
       console.log("Length all lucrative paths passed: ", allLucrPathsPassed.length);
       console.log(`-------Total number of executions: ${execCount}\n`);
